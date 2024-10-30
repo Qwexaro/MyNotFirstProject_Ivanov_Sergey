dbb6295fc70b1089f73897d20be756d84dd581c2
Sergey0909087 <Sergikstar25.gold@mail.ru>
2024-10-30 13:01:22 +0300

    main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ivanov_branch");
+        System.out.println("Введи число я покажу его множителеи: ");
+        int x = new Scanner(System.in).nextInt();
+        for (int i = 1; i <= x; i++)
+        {
+            for(int j = x; j >= 1; j--)
+            {
+                if(i * j == x)
+                {
+                    System.out.println(i + " * " + j);
+                }
+            }
+        }
     }
 }
